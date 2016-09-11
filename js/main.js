b04ac6c05b65f7e306ec31a589f8ee7f332ce899
Jeff Moss <mosspilot@gmail.com>
2016-09-10 18:32:11 -0700

    bearer token and request work

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/* Function used to fetch data and call appropriate js method*/
+var _token
+ 
+var fetchToken = function() {
+  var clientId = 'Ay2Z4XGewvq7IWYsUzusV0RejU6U4L2z'
+  var clientSecret = 'vjbFZVzxU5zPK54x'
+  var accessTokenUri = 'https://api.yaas.io/hybris/oauth2/v1/token'
+  var req = new XMLHttpRequest();
+  if (!req) {
+    throw 'Unable to create HTTP Request.';
+  }
+  var url = accessTokenUri
+  var params = 'grant_type=client_credentials' + '&client_id=' + clientId + '&client_secret=' + clientSecret + '&scope=hybris.order_read'
+  req.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        var _jsonObj = JSON.parse(this.responseText);
+        _token = _jsonObj.access_token
+        getOrders()
+      } else {
+        alert('There was a problem with the request');
+      }
+    }
+  };
+  req.open('POST', url, true);
+  req.setRequestHeader('content-type', 'application/x-www-form-urlencoded')
+  req.send(params);
+};
+
+//Fetch data from the order datapoint
+var getOrders = function() {
+  var req = new XMLHttpRequest();
+  if (!req) {
+    throw 'Unable to create HTTP Request.';
+  }
+  var url = 'https://api.yaas.io/hybris/order/v1/businessinsights/salesorders'
+  req.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        var response = req.responseText;
+        var innerdiv = document.getElementById('responsediv');
+        innerdiv.innerHTML = JSON.parse(response)
+        var _jsonObj = JSON.parse(response);
+        var data = [
+    {"Product": "T-shirt", "name":"T-shirt", "Quantity": 15},
+    {"Product": "Shoes", "name":"Shoes", "Quantity": 10},
+    {"Product": "Towel", "name":"Towel", "Quantity": 20},
+    {"Product": "Hats", "name":"Hats", "Quantity": 32}
+  ]
+  var visualization = d3plus.viz()
+    .container("#viz")
+    .data(data)
+    .type("bar")
+    .id("name")
+    .x("Product")
+    .y("Quantity")
+    .draw()
+      } else {
+        alert('There was a problem with the request');
+      }
+    }
+  };
+  req.open('GET', url, true);
+  req.setRequestHeader('content-type', 'application/x-www-form-urlencoded')
+  req.setRequestHeader('Authorization', 'Bearer ' + _token)
+  req.send();
+};
+
+window.onload = function () {
+  fetchToken();
+};
